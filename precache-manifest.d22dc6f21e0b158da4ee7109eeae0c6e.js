95dd69785b7ebffba60bdaa9a24871d0558bc5c9
KingRumble <jalenkincaid0@gmail.com>
2019-12-08 19:15:43 -0600

    Updates

---
+++ precache-manifest.d22dc6f21e0b158da4ee7109eeae0c6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb4ee665b11d340541763ca60a74433",
+    "url": "/GoblinWebsite/index.html"
+  },
+  {
+    "revision": "0fd7d75710d32334307b",
+    "url": "/GoblinWebsite/static/css/main.f9d96440.chunk.css"
+  },
+  {
+    "revision": "8326e61622f8f46d910c",
+    "url": "/GoblinWebsite/static/js/2.1e284c41.chunk.js"
+  },
+  {
+    "revision": "0fd7d75710d32334307b",
+    "url": "/GoblinWebsite/static/js/main.450bc42c.chunk.js"
+  },
+  {
+    "revision": "c31d3c0d0d86d74af32a",
+    "url": "/GoblinWebsite/static/js/runtime-main.3b5e783b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/GoblinWebsite/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "7df2ab81159863144c9e4ccf7d216a26",
+    "url": "/GoblinWebsite/static/media/volcano3.7df2ab81.jpeg"
+  },
+  {
+    "revision": "8aa3e958bc73bce01c38f91ee0bc4d02",
+    "url": "/GoblinWebsite/static/media/volcano5.8aa3e958.jpg"
+  }
+]);
